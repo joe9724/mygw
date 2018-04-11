a1541392471929b48483053970a6f9d618fab7fe
joe9724 <64060764@qq.com>
2018-04-11 23:25:35 +0800

    gg

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func ReverseProxyHandler(ctx *fasthttp.RequestCtx) {
 		ctx.Logger().Printf("error when proxying the request: %s", err)
 	}
 
-	postprocessResponse(ctx,resp)
+	postprocessResponse(resp)
 }
 
 func prepareRequest(req *fasthttp.Request) {
@@ -43,7 +43,7 @@ func prepareRequest(req *fasthttp.Request) {
 	req.SetRequestURI("http://www.baidu.com")
 }
 
-func postprocessResponse(ctx *fasthttp.RequestCtx,resp *fasthttp.Response) {
+func postprocessResponse(resp *fasthttp.Response) {
 	// do not proxy "Connection" header
 	resp.Header.Del("Connection")
 
@@ -53,31 +53,6 @@ func postprocessResponse(ctx *fasthttp.RequestCtx,resp *fasthttp.Response) {
 	// resp.Header.Set("Access-Control-Allow-Origin", "*")
 	// resp.Header.Set("Access-Control-Request-Method", "OPTIONS,HEAD,POST")
 	// resp.Header.Set("Content-Type", "application/json; charset=utf-8")
-	fmt.Fprintf(ctx, "Hello, world!\n\n")
-
-	fmt.Fprintf(ctx, "Request method is %q\n", ctx.Method())
-	fmt.Fprintf(ctx, "RequestURI is %q\n", ctx.RequestURI())
-	fmt.Fprintf(ctx, "Requested path is %q\n", ctx.Path())
-	fmt.Fprintf(ctx, "Host is %q\n", ctx.Host())
-	fmt.Fprintf(ctx, "Query string is %q\n", ctx.QueryArgs())
-	fmt.Fprintf(ctx, "User-Agent is %q\n", ctx.UserAgent())
-	fmt.Fprintf(ctx, "Connection has been established at %s\n", ctx.ConnTime())
-	fmt.Fprintf(ctx, "Request has been started at %s\n", ctx.Time())
-	fmt.Fprintf(ctx, "Serial request number for the current connection is %d\n", ctx.ConnRequestNum())
-	fmt.Fprintf(ctx, "Your ip is %q\n\n", ctx.RemoteIP())
-
-	fmt.Fprintf(ctx, "Raw request is:\n---CUT---\n%s\n---CUT---", &ctx.Request)
-
-	ctx.SetContentType("text/plain; charset=utf8")
-
-	// Set arbitrary headers
-	ctx.Response.Header.Set("X-My-Header", "my-header-value")
-
-	// Set cookies
-	var c fasthttp.Cookie
-	c.SetKey("cookie-name")
-	c.SetValue("cookie-value")
-	ctx.Response.Header.SetCookie(&c)
 }
 
 func main() {
@@ -93,4 +68,5 @@ func main() {
 	if err := fasthttp.ListenAndServe("localhost:"+*port, ReverseProxyHandler); err != nil {
 		log.Fatalf("error in fasthttp server: %s", err)
 	}
+	fmt.Println("start server...")
 }
